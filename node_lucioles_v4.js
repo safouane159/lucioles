70fe49fa348ed777eaf72c51c2e282d476edcd93
safouaneouazri <safouane1ouazri@gmail.com>
2022-04-09 13:52:29 +0200

    fkn

--- node_lucioles_v4.js
+++ node_lucioles_v4.js
@@ -220,7 +220,7 @@ app.get('/esp/list', function (req, res) {
 app.get('/geogs/:what', function (req, res) {
 	esp_mac_address = req.params.what
 	if (esp_mac_address === "80:7D:3A:FD:CF:68"){
-		var data = { name: 'Location B', category: 'House', street: 'Broad', lat: 39.284, lng: -75.833 },
+		var data = { name: 'Location B', category: 'House', street: 'Broad', lat: 39.284, lng: -75.833 };
   
 			}else{
 		var data = { name: 'Location A', category: 'Store', street: 'Market', lat: 39.984, lng: -75.343 };
