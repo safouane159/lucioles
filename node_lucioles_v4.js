144b8d4b7e513c6254ee79cd536dfd5a9f38e9fa
safouaneouazri <safouane1ouazri@gmail.com>
2022-04-10 13:41:05 +0200

    nk

--- node_lucioles_v4.js
+++ node_lucioles_v4.js
@@ -58,19 +58,19 @@ async function v0(){
 	dbo = mg_client.db(mongoName);
 
 	// Remove "old collections : temp and light
-	dbo.listCollections({name: "temp"})
+	dbo.listCollections({name: "localisation"})
 	    .next(function(err, collinfo) {
 		if (collinfo) { // The collection exists
 		    //console.log('Collection temp already exists');
-		    dbo.collection("temp").drop() 
+		    dbo.collection("localisation").drop() 
 		}
 	    });
 
-	dbo.listCollections({name: "light"})
+	dbo.listCollections({name: "sensors"})
 	    .next(function(err, collinfo) {
 		if (collinfo) { // The collection exists
 		    //console.log('Collection temp already exists');
-		    dbo.collection("light").drop() 
+		    dbo.collection("sensors").drop() 
 		}
 	    });
 
