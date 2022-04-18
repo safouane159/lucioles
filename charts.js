16444f0bd2dc159b0030a216db6b85bf843dc1a5
safouaneouazri <safouane1ouazri@gmail.com>
2022-04-18 11:59:58 +0200

    lets make lucioles great again

--- charts.js
+++ charts.js
@@ -50,7 +50,7 @@ $(function() {
         event.preventDefault();
         let form = document.getElementById('myform');
        // console.log('inside prevent'+$(this).what.val() );
-        console.log('inside preventval'+form.elements["what"] );
+        console.log('inside preventval'+form.elements["what"].value );
         node_url = 'https://lucioles.herokuapp.com';
         
          $.ajax({
