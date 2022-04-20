fa7eaf6c073526648f5fdefd3e7810871c48f13a
safouaneouazri <safouane1ouazri@gmail.com>
2022-04-20 12:15:25 +0200

    lets make lucioles great again

--- node_lucioles_v4.js
+++ node_lucioles_v4.js
@@ -395,7 +395,7 @@ var transporter = nodemailer.createTransport({
 		console.log("the key ", key);
 		
 		var autirized = false;
-		key = "cr";
+		key = "7a75439dfd102591af41d0ac1f1df7d62cc594f5";
 
 		dbo.collection("keys").findOne({key:key},function(err, result) {
 			if (err) throw err;
