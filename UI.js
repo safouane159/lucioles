a763a0c03e8f8ef150135f6b26f5e4187c4e4003
safouaneouazri <safouane1ouazri@gmail.com>
2022-04-21 14:07:00 +0200

    lets make lucioles great again

---
+++ UI.js
@@ -0,0 +1,41 @@
+$(function() {
+    $('#inscription').submit(function(event) {
+        event.preventDefault();
+        //generaing random SHA1 hash
+        
+       // console.log('inside prevent'+$(this).what.val() );
+        console.log('inside function js inscription');
+        let form = document.getElementById('inscription');
+        // console.log('inside prevent'+$(this).what.val() );
+        var Name = form.elements["name"].value;
+        var Email = form.elements["email"].value;
+        var Mdps = form.elements["mdps"].value;
+         console.log('inside preventval'+form.elements["what"].value );
+
+        node_url = 'https://lucioles.herokuapp.com';
+        $.ajax({
+            url: node_url.concat('/inscription'), // URL to "GET" : /esp/temp ou /esp/light
+            type: 'POST',
+            data: {
+                name: Name,
+                Email: Email, 
+                Mdps: Mdps // Second add quotes on the value.
+              },
+    
+            success: function (resultat, statut) { // Anonymous function on success
+                console.log("Envoi key to server results : "+resultat)
+                
+                
+                
+                
+               
+              
+            },
+            error: function (resultat, statut, erreur) {
+                console.log("Envoi key to server statut : "+statut+"resultat"+resultat+"erreur : "+erreur) 
+            },
+            complete: function (resultat, statut) {
+                console.log("Envoi key to server statut : "+statut+"resultat"+resultat)
+            } });
+    });
+})
